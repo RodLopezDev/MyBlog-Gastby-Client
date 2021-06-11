9ba32727cc042beee80162fb7d4e2cb8fd85f414
Rodrigo <rodrigo.lopez@continuum.cl>
2021-06-11 11:46:53 -0500

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <div>
+    <Seo title="Home" />
+    <div>Hola mundo</div>
+  </div>
+)
+
+export default IndexPage
